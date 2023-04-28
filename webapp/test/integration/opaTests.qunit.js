84602e18e3208fb062ebe9b098b3111d1593cbd7
Dayeong <62468132+dayeong918@users.noreply.github.com>
2023-04-28 17:33:18 +0900

    Add files via upload

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zprojectodatae0401/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zprojectodata_e04_01/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
